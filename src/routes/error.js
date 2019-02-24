7d398f15265a9dd77823cf1fcd05e70b96a7363a
Mitch Pierias <mitch@pierias.com>
2019-02-24 23:37:26 +1000

    Made functional

--- src/routes/error.js
+++ src/routes/error.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
+export default function NotFound({ history }) {
 
-    render() {
-        return (
-            <div>
-                <h2>Not Found</h2>
-                <p>Oops... The page you requested can't be found</p>
-                <button className="button icon-left" onClick={this.props.history.goBack}>Go Back</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>Oops... The page you requested can't be found</p>
+            <button className="button icon-left" onClick={history.goBack}>Go Back</button>
+        </div>
+    )
 }
